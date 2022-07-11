ab5230e7024c87c18ab29dbbd43cc2efb6b3453b
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2022-07-11 20:48:26 -0300

    add Ribbon to Banner implementation

--- src/components/Banner/stories.tsx
+++ src/components/Banner/stories.tsx
@@ -4,6 +4,11 @@ import Banner, { BannerProps } from '.'
 export default {
   title: 'Banner',
   component: Banner,
+  argTypes: {
+    ribbon: {
+      type: 'string'
+    }
+  },
   args: {
     img: 'https://images.unsplash.com/photo-1629760946220-5693ee4c46ac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
     title: 'Defy death',
@@ -16,4 +21,20 @@ export default {
   }
 } as Meta
 
-export const Default: Story<BannerProps> = (args) => <Banner {...args} />
+export const Default: Story<BannerProps> = (args) => (
+  <div style={{ maxWidth: '104rem', margin: '0 auto' }}>
+    <Banner {...args} />
+  </div>
+)
+
+export const WithRibbon: Story<BannerProps> = (args) => (
+  <div style={{ maxWidth: '104rem', margin: '0 auto' }}>
+    <Banner {...args} />
+  </div>
+)
+
+WithRibbon.args = {
+  ribbon: '20% OFF',
+  ribbonSize: 'normal',
+  ribbonColor: 'primary'
+}
