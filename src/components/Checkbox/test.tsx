5391ac9054edcc13411e7360385b239a7d3edb0d
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2022-08-22 21:52:37 -0300

    add Checkbox component

---
+++ src/components/Checkbox/test.tsx
@@ -0,0 +1,60 @@
+import { screen, waitFor } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+import userEvent from '@testing-library/user-event'
+
+import theme from 'styles/theme'
+import Checkbox from '.'
+
+describe('<Checkbox />', () => {
+  it('should render with label', () => {
+    renderWithTheme(<Checkbox label="checkbox label" labelFor="check" />)
+
+    expect(screen.getByRole('checkbox')).toBeInTheDocument()
+    expect(screen.getByLabelText(/checkbox label/i)).toBeInTheDocument()
+    expect(screen.getByText(/checkbox label/i)).toHaveAttribute('for', 'check')
+  })
+
+  it('should render without label', () => {
+    renderWithTheme(<Checkbox />)
+
+    expect(screen.queryByLabelText(/checkbox/i)).not.toBeInTheDocument()
+  })
+
+  it('should render black label', () => {
+    renderWithTheme(
+      <Checkbox label="checkbox label" labelFor="check" labelColor="black" />
+    )
+
+    expect(screen.getByText(/checkbox label/i)).toHaveStyle({
+      color: theme.colors.black
+    })
+  })
+
+  it('should dispatch onCheck when status changes', async () => {
+    const onCheck = jest.fn()
+
+    renderWithTheme(<Checkbox label="checkbox" onCheck={onCheck} />)
+
+    expect(onCheck).not.toHaveBeenCalled()
+
+    userEvent.click(screen.getByRole('checkbox'))
+    await waitFor(() => {
+      expect(onCheck).toHaveBeenCalledTimes(1)
+    })
+    expect(onCheck).toHaveBeenCalledWith(true)
+  })
+
+  it('should dispatch onCheck when status changes', async () => {
+    const onCheck = jest.fn()
+
+    renderWithTheme(<Checkbox label="checkbox" onCheck={onCheck} isChecked />)
+
+    expect(onCheck).not.toHaveBeenCalled()
+
+    userEvent.click(screen.getByRole('checkbox'))
+    await waitFor(() => {
+      expect(onCheck).toHaveBeenCalledTimes(1)
+    })
+    expect(onCheck).toHaveBeenCalledWith(false)
+  })
+})
