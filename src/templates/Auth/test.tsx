b658607122340c1b12dd4bbbdd978f2dcc6f9275
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2022-09-01 21:40:59 -0300

    add Atuh template

---
+++ src/templates/Auth/test.tsx
@@ -0,0 +1,35 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Auth from '.'
+
+describe('<Auth />', () => {
+  it('should render all components and children', () => {
+    renderWithTheme(
+      <Auth title="Auth title">
+        <input type="text" />
+      </Auth>
+    )
+
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
+    expect(
+      screen.getByRole('heading', {
+        name: /All your favorite games in one place/i
+      })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', {
+        name: /won is the best an most complete gaming platform/i
+      })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', {
+        name: /auth title/i
+      })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('textbox')).toBeInTheDocument()
+  })
+})
