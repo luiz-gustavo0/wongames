82c1ebdb8ff6f1036b1cbe39bebb07b6e409a95f
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2022-10-17 20:50:49 -0300

    add ProfileMenu

---
+++ src/components/ProfileMenu/stories.tsx
@@ -0,0 +1,11 @@
+import { Story, Meta } from '@storybook/react'
+import ProfileMenu, { ProfileMenuProps } from '.'
+
+export default {
+  title: 'ProfileMenu',
+  component: ProfileMenu
+} as Meta
+
+export const Default: Story<ProfileMenuProps> = (args) => (
+  <ProfileMenu {...args} />
+)
