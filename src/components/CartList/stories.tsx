1b23a71e1f57fd03ea2ab3fe2b4d97c8f1679a0a
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2023-04-19 22:59:01 -0300

    update CartList to have hasButton property

--- src/components/CartList/stories.tsx
+++ src/components/CartList/stories.tsx
@@ -1,4 +1,4 @@
-import { Story, Meta } from '@storybook/react/types-6-0'
+import { Story, Meta } from '@storybook/react'
 import CartList, { CartListProps } from '.'
 
 import mockItems from './mock'
@@ -10,11 +10,6 @@ export default {
     items: mockItems,
     total: 'R$ 330,00'
   },
-  argTypes: {
-    items: {
-      type: ''
-    }
-  },
   parameters: {
     backgrounds: {
       default: 'won-dark'
@@ -25,5 +20,11 @@ export default {
 export const Default: Story<CartListProps> = (args) => (
   <div style={{ maxWidth: 800 }}>
     <CartList {...args} />
+  </div>
+)
+
+export const WithButton: Story<CartListProps> = (args) => (
+  <div style={{ maxWidth: 800 }}>
+    <CartList {...args} hasButton />
   </div>
 )
