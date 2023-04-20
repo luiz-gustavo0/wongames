70caf5936b9b33778ad5e1aaab8b36ec97c19c07
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2023-04-20 00:43:36 -0300

    add UserDropdown component

---
+++ src/components/UserDropdown/test.tsx
@@ -0,0 +1,26 @@
+import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import UserDropdown from '.'
+
+describe('<UserDropdown />', () => {
+  it('should render the username', () => {
+    renderWithTheme(<UserDropdown username="Luiz" />)
+
+    expect(screen.getByText(/luiz/i)).toBeInTheDocument()
+  })
+
+  it('should render the menu', async () => {
+    renderWithTheme(<UserDropdown username="Luiz" />)
+
+    await userEvent.click(screen.getByText(/luiz/i))
+
+    expect(
+      screen.getByRole('link', { name: /my profile/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('link', { name: /wishlist/i })).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: /sign out/i })).toBeInTheDocument()
+  })
+})
